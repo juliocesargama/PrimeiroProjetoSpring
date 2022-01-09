c294fcdb6da6f5dc12299065cb9d6e8ad17e86da
Julio César Gama <juliocesargama@protonmail.com>
2022-01-08 23:20:02 -0300

    Home Controller + Inclusão do template de exemplo

--- src/main/java/br/com/springboot/controllers/HomeController.java
+++ src/main/java/br/com/springboot/controllers/HomeController.java
@@ -1,13 +1,17 @@
 package br.com.springboot.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+
+        model.addAttribute("nome", "Julio");
+
         return "home/index";
     }
     
